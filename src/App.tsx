1c21761b363b8eeede88402d91018a69ccdadbb4
Shoaib <shoaibchemistrysust@gmail.com>
2026-09-11 20:03:57 +0600

    project setup was completed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h2 className='text-3xl'>Welcome to TailwindCSS</h2>
+     <button className="btn btn-success">Welcome to DaisyUI</button>
+    </>
+  )
+}
+
+export default App
